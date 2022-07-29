9ecb591310dc0f2b8f0d585e83681e35e46dc9f6
andrew <sanakurin@gmail.com>
2022-07-29 16:45:56 +0300

    created building algo for ThreadModule

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ cmake CMakeLists.txt
 
 make
 
+build/modules/ThreadModule/build.sh
+
 #mv build/app_bin.bin build/build-output
---
+++ build/modules/ThreadModule/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Moving build output files to build-output for ThreadModule" 
+
+mv build/modules/ThreadModule/thread_mo_tst_app.bin build/build-output
